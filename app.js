e42b1f22f7fcd2c1de45fc3d66365aec7dba762d
Bolsarim <giovanna.bolsarim@gmail.com>
2023-12-19 13:42:34 -0300

    lojaamigurumi

---
+++ app.js
@@ -0,0 +1,14 @@
+function abrirtigre() {
+    // Abrir a página queijo.html em uma nova janela ou guia
+    window.open('tigre.html', '_blank');
+    // Fechar a janela ou guia atual (pode não funcionar em alguns navegadores devido a restrições de segurança)
+    window.close();
+}
+function abrirursinho() {
+    window.open('ursinho.html', '_blank');
+    window.close();
+
+}
+function abrircoala() {
+    window.open('coala.html', '_blank');
+}
